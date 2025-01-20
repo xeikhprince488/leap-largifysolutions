d93f4abf6f0191e246e22257988079977918cee2
xeikhprince488 <programmingworld488@gmail.com>
2025-01-21 02:00:55 +0500

    dlt mcqs too0000

--- pages/api/questions/delete.ts
+++ pages/api/questions/delete.ts
@@ -23,7 +23,7 @@ export default async function handler(req: { method: string; query: { id: any, g
 
     const result = await collection.updateOne(
       { $or: [{ "short.id": parsedId }, { "mcq.id": parsedId }, { "long.id": parsedId }] },
-      { $pull: { short: { id: parsedId }, mcq: { id: parsedId }, long: { id: parsedId } } }
+      { $pull: { short: { id: parsedId }, mcq: { id: parsedId }, long: { id: parsedId } } as any }
     )
 
     console.log('Delete result:', result) // Debugging line
